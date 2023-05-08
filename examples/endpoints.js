285136ba52429f14cbe85dccfd7b91f8bcb0a18b
Wesley Buurke <w.buurke@enie.nl>
2023-05-08 13:10:55 +0200

    fix: console log calls

--- examples/endpoints.js
+++ examples/endpoints.js
@@ -5,24 +5,32 @@ function makeEndpoints(app, HLTV) {
   })
 
   app.get('/results', async (_req, res) => {
+    console.log(`Calling /results`)
     const results = await HLTV.getResults()
+    console.log(`Called /results`)
     res.json(results)
   })
 
   app.get('/matches', async (_req, res) => {
+    console.log(`Calling /matches`)
     const matches = await HLTV.getMatches()
+    console.log(`Called /matches`)
     res.json(matches)
   })
 
   app.get('/matches/:eventId', async (req, res) => {
     const { eventId } = req.params
+    console.log(`Calling /matches/${eventId}`)
     const matches = await HLTV.getMatches(eventId)
+    console.log(`Called /matches/${eventId}`)
     res.json(matches)
   })
 
   app.get('/results/:matchId/stats', async (req, res) => {
     const { matchId } = req.params
+    console.log(`Calling /results/${matchId}/stats`)
     const match = await HLTV.getMatchById(matchId)
+    console.log(`Called /results/${matchId}/stats`)
     res.json(match)
   })
 
